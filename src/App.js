4b4ef2e87a18a4aac0fa440c3fb26d34684be65b
Harish Bohara <harish.bohara@gmail.com>
2022-01-25 23:43:36 +0530

    added nav bar to list all items

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import Button from 'react-bootstrap/Button';
+import Home from './components/home/home'
 
 
 function App() {
   return (
     <>
-      <Button variant="primary">Primary</Button>{' '}
-      <Button variant="secondary">Secondary</Button>{' '}
-      <Button variant="success">Success</Button>{' '}
-      <Button variant="warning">Warning</Button>{' '}
-      <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
-      <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
-      <Button variant="link">Link</Button>
+      <Home></Home>     
     </>
   );
 }
---
+++ src/components/home/home.js
@@ -0,0 +1,19 @@
+import { Container } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+    return (
+        <Navbar bg="primary" variant="dark">
+            <Container fluid>
+                <Navbar.Brand href="#home">MfFlowX</Navbar.Brand>
+                    <Nav className="me-auto">
+                    <Nav.Link href="#deployments">Deployments</Nav.Link>
+                    <Nav.Link href="#models">Models</Nav.Link>
+                    <Nav.Link href="#help">Help</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+  )
+}
