75167fce9011bb2c23ad383b2bb524ad64f96dff
Harish Bohara <harish.bohara@gmail.com>
2022-01-26 00:51:40 +0530

    added nav bar to list all items

--- src/components/deployments/DeploymentList.js
+++ src/components/deployments/DeploymentList.js
@@ -4,39 +4,21 @@ import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
 
-const elements = [
-    {
-      id: '1',
-      data: { label: 'Login Model', link: "/login/model" },
-    },
-    {
-      id: '2',
-      data: { label: 'Logout Model', link: "/loginout/model" },
-    },
-    {
-      id: '3',
-      data: { label: 'Payment Model', link: "/payment/model" },
-    },    
-  ];
-
-
 // Left menu style
 const style = {width: '100%',maxWidth: 360,bgcolor: 'background.paper',};
   
 // eslint-disable-next-line import/no-anonymous-default-export
-export default() => {
+export default({deployments, setDeployment}) => {
 
-    // These are the deployments - TODO (get if from outside)
-    const [deployments] = useState(elements);
 
     // Add all models in left menu
     const menuItems = deployments.map((deployment) =>
-        <>
-         <ListItem button>
-            <ListItemText primary={deployment.data.label} />
-            </ListItem>
+        <div key={deployment.id}>
+            <ListItem  button>
+                <ListItemText primary={deployment.data.label} onClick={() => setDeployment(deployment)} />            
+            </ListItem>    
             <Divider />
-        </>
+        </div>
     );
 
     // Final component
--- src/components/deployments/Deployments.js
+++ src/components/deployments/Deployments.js
@@ -1,17 +1,44 @@
 /* eslint-disable import/no-anonymous-default-export */
 
+import React, { useState, useEffect } from 'react';
 import { Container } from "react-bootstrap"
 import { Row } from "react-bootstrap"
 import { Col } from "react-bootstrap"
 import DeploymentList from "./DeploymentList"
-import Deployment
- from "./Deployment"
+import Deployment from "./Deployment"
+
+const elements = [
+    {
+      id: '1',
+      data: { label: 'Login Model', link: "/login/model" },
+    },
+    {
+      id: '2',
+      data: { label: 'Logout Model', link: "/loginout/model" },
+    },
+    {
+      id: '3',
+      data: { label: 'Payment Model', link: "/payment/model" },
+    },
+    {
+      id: '4',
+      data: { label: 'Bad Model', link: "/payment/model" },
+    },    
+  ];
+
 export default() => {
+
+    // These are the deployments - TODO (get if from outside)
+    const [deployments] = useState(elements);
+
+    // This is the deployment which will be shown
+    const [deployment, setDeployment] = useState(null);
+
     return(
         <Container>
             <Row>
-                <Col xs={3}><DeploymentList></DeploymentList></Col>
-                <Col xs={6}><Deployment></Deployment></Col>
+                <Col xs={3}><DeploymentList deployments={deployments} setDeployment={setDeployment}></DeploymentList></Col>
+                <Col xs={6}><Deployment deployment={deployment}></Deployment></Col>
             </Row>      
         </Container>
     )
