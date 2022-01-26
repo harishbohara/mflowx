0fe49904078a25de48244498a7d2a5259070abd4
Harish Bohara <harish.bohara@gmail.com>
2022-01-27 01:53:21 +0530

    added nav bar to list all items

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,12 +1,12 @@
-import { Box } from "@mui/material"
+import { Container } from "@mui/material"
 import DeploymentsContainer from "../deployments/v1/DeploymentsContainer"
 import Menu from '../menu/Menu'
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
     return (
-        <Box sx={{ width: '80%', minWidth: 800, bgcolor: 'background.paper' }}>
+        <Container >
             <Menu></Menu>
             <DeploymentsContainer />
-        </Box>
+        </Container>
     )
 }
