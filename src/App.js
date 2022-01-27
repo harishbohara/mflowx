ff90f12c47b107af42f833883c9444b54cf774ad
Harish Bohara <harish.bohara@gmail.com>
2022-01-27 21:01:04 +0530

    added fetch data support

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './components/home/home'
+import Home from './components/home/Home'
 
 
 function App() {
   return (
     <>
-      <Home></Home>     
+      <Home></Home>
     </>
   );
 }
-
 export default App;
